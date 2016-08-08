4cbab03dccbb66d5755f14cf6fa2d7cfdc17f009
Hartmut Keller <keller@fs-net.de>
2016-08-08 17:56:38 +0200

    Improve gpio example
    - Command line now uses gpio number first, then the optional direction
      as 'in', 'out', 'low' or 'high', then the optional loop count, then
      the optional loop delay. Modify usage text accordingly.
    - Using streams simplifies the code considerably.
    - Also open directory when accessing the gpio files.
    - Do everything in main(), no need to call separate functions.
    - Improve error handling and comments.

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -1,4 +1,4 @@
-//*****************************************************************************/
+/*****************************************************************************/
 /***     ______       _____    ______                           _          ***/
 /***    |  ____|__   / ____|  |  ____|                         | |         ***/
 /***    | |__ ( _ ) | (___    | |__  __  ____ _ _ __ ___  _ __ | | ___     ***/
@@ -9,24 +9,26 @@
 /*****************************************************************************/
 /***                                                                       ***/
 /***                                                                       ***/
-/***                           GPIO EXAMPLE                      	   ***/
+/***                           GPIO EXAMPLE                                ***/
 /***                                                                       ***/
 /***                                                                       ***/
 /*****************************************************************************/
 /*** File:     gpio.c                                                      ***/
-/*** Author:   C. Canbaz                                                   ***/
+/*** Author:   C. Canbaz, P. Jakob                                         ***/
 /*** Created:  30.10.2015                                                  ***/
-/*** Modified: 24.06.2016 PJ                                               ***/
+/*** Modified: 08.08.2016 17:53:00 (HK)                                    ***/
 /***                                                                       ***/
 /*** Description                                                           ***/
 /*** -----------                                                           ***/
-/*** Show how the GPIO ports are used on F&S Boards.			   ***/
+/*** Show how the GPIO ports are used on F&S Boards.                       ***/
 /***                                                                       ***/
 /*** Compile with:                                                         ***/
 /***              arm-linux-gcc -o gpio gpio.c                             ***/
 /***                                                                       ***/
 /*** Modification History:                                                 ***/
-/*** 24.06.2016 PJ: Improve Code now the gpio can work for all boards      ***/
+/*** 24.06.2016 PJ: Improve Code now the gpio can work for all boards.     ***/
+/*** 08.08.2016 HK: Simplify code, use streams, command line now has gpio  ***/
+/***                first, then in/out/low/high, then count, then delay.   ***/
 /*****************************************************************************/
 /*** THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY ***/
 /*** KIND,  EITHER EXPRESSED OR IMPLIED,  INCLUDING BUT NOT LIMITED TO THE ***/
@@ -34,158 +36,43 @@
 /*** PURPOSE.                                                              ***/
 /*****************************************************************************/
 
-#include <stdlib.h>		/* strtoul() */
-#include <stdio.h>		/* perror() */
-#include <unistd.h>		/* write(), close(), fsync(), sleep() */
-#include <fcntl.h>		/* open(), O_WRONLY */
-#include <string.h>		/* strlen() */
+#include <stdlib.h>			/* strtoul() */
+#include <stdio.h>			/* fprintf(), sprintf(), perror() */
+#include <unistd.h>			/* sleep() */
+#include <dirent.h>			/* DIR, opendir() */
+#include <string.h>			/* strcmp() */
+#include <limits.h>			/* PATH_MAX, MAX_INT */
+#include <errno.h>			/* errno, ENOENT */
 
+#define GPIO_PATH	"/sys/class/gpio"
+#define EXPORT_PATH	"/sys/class/gpio/export"
 
-/* File paths */
-#define SYS_EXP "/sys/class/gpio/export"
-#define SYS_ADD "/sys/class/gpio/gpio??"
-#define SYS_DIR "/direction"
-#define SYS_VAL "/value"
+static char path_gpio[PATH_MAX];
+static char path[PATH_MAX];
 
 
 /*****************************************************************************
-*** Function:    int getFileDeskriptor(const char *file_path,              ***
-***                              const int MODE)                           ***
+*** Function:    int show_error(char *reason, char *bad_path)              ***
 ***                                                                        ***
-*** Parameters:  file_path: Path from the file, which want to modifying.   ***
-***              MODE:      MODE for open the filedescriptor with          ***
-***                         read or write permissions                      ***
+*** Parameters:  reason:   Pointer to string with error reason             ***
+***              bad_path: Optional pointer to path (added as second       ***
+***                        output line if not NULL)                        ***
 ***                                                                        ***
-*** Return:      fd:        Filedescriptor                                 ***
+*** Return:      1: Failure; value is meant as final program status        ***
 ***                                                                        ***
 *** Description                                                            ***
 *** -----------                                                            ***
-*** This function open the filedeskritpor and returns it.                  ***
+*** Print error reason, actual error (from errno) and (if not NULL) the    ***
+*** given path. This function always returns 1, which is meant as program  ***
+*** status at progam end.                                                  ***
 *****************************************************************************/
-int getFileDeskriptor(const char *file_path, const int MODE)
+static int show_error(const char *reason, const char *bad_path)
 {
-	int fd = open(file_path, MODE);
+	perror(reason);
+	if (bad_path)
+		fprintf(stderr, "Bad path: %s\n", bad_path);
 
-	if(fd<0)
-	{
-		perror(file_path);
-		return 1;
-	}	
-	return fd;
-}
-
-
-/*****************************************************************************
-*** Function:    int createGPIO(const char *pinNr)                         ***
-***                                                                        ***
-*** Parameters:  pinNr: Number of GPIO pin                                 ***
-***                                                                        ***
-*** Return:      -                                                         ***
-***                                                                        ***
-*** Description                                                            ***
-*** -----------                                                            ***
-*** Creates the GPIO files in /sys/class/gpio.                             ***
-*****************************************************************************/
-int createGPIO(const char *pinNr)
-{
-	// Open Filedescriptor
-	int fd = getFileDeskriptor(SYS_EXP, O_WRONLY);
-
-	// Create GPIO Files
-	printf("Use GPIO%s\n",pinNr);
-	write(fd,pinNr,sizeof(pinNr));
-	fsync(fd);
-	close(fd);
-	return 0;
-}
-
-
-/*****************************************************************************
-*** Function:    int set Direction(const char *pinNr, const char *outin)   ***
-***                                                                        ***
-*** Parameters:  pinNr: Number of GPIO pin                                 ***
-***              outin: To be set direction as output or input             ***
-***                                                                        ***
-*** Return:      -                                                         ***
-***                                                                        ***
-*** Description                                                            ***
-*** -----------                                                            ***
-*** This function sets the direction of the GPIO port as input or output.  ***
-*****************************************************************************/
-void setDirection(const char *pinNr,const char *outin)
-{	
-	char file_path[] = SYS_ADD SYS_DIR;
-
-	file_path[strlen(SYS_ADD) - 2] = *pinNr;
-	file_path[strlen(SYS_ADD) - 1] = *(pinNr+1);
-	int fd=getFileDeskriptor(file_path,O_WRONLY);
-
-	//Configuration Direction Output/Input
-	printf("GPIO%s als %sput konfiguriert\n",pinNr,outin);	
-	write(fd,outin,sizeof(outin));		
-	fsync(fd);			
-	close(fd);			 
-}
-
-
-/*****************************************************************************
-*** Function:    int setValue(const char *pinNr, const char *value)        ***
-***                                                                        ***
-*** Parameters:  pinNr: Number of GPIO pin                                 ***
-***              value: To be set the value for the GPIO port as output    ***
-***                                                                        ***
-*** Return:      -                                                         ***
-***                                                                        ***
-*** Description                                                            ***
-*** -----------                                                            ***
-*** This function sets the value of the GPIO port, which used as output.   ***
-*****************************************************************************/
-void setValue(const char *pinNr, const char *value)
-{
-	char file_path[] = SYS_ADD SYS_VAL;
-
-	file_path[strlen(SYS_ADD) - 2] = *pinNr;
-	file_path[strlen(SYS_ADD) - 1] = *(pinNr+1);
-
-	int fd=getFileDeskriptor(file_path,O_WRONLY);
-
-	printf("GPIO%s Wert auf %s\n", pinNr, value);
-	write(fd,value,sizeof(value));
-	fsync(fd);
-	sleep(1);
-	close(fd);
-}
-
-
-/*****************************************************************************
-*** Function:    int getValue(char *pinNr)                                 ***
-***                                                                        ***
-*** Parameters:  pinNr:        Number of GPIO pin                          ***
-***                                                                        ***
-*** Return:      return_value: Input value of GPIO                         ***
-***                                                                        ***
-*** Description                                                            ***
-*** -----------                                                            ***
-*** This function gives the input value, which was read.                   ***
-*****************************************************************************/
-int getValue(const char *pinNr)
-{
-	/* create the correct path */
-	char file_path[] = SYS_ADD SYS_VAL;
-
-	file_path[strlen(SYS_ADD) - 2] = *pinNr;
-	file_path[strlen(SYS_ADD) - 1] = *(pinNr+1);
-
-	/* give filedescriptor */	
-	int fd=getFileDeskriptor(file_path, O_RDONLY);
-
-	/* read value */
-	char value[2];
-	read (fd,&value,sizeof(value));
-	close (fd);
-	int return_value;
-	return_value = (int)value[0];
-	return return_value;
+	return 1;
 }
 
 
@@ -203,85 +90,13 @@ int getValue(const char *pinNr)
 void usage(const char *progname)
 {
 	printf("\n"
-	       "Usage: %s <direction> <gpio_nr> [count] [delay]\n"
+	       "Usage: %s gpio [dir [count [delay]]]\n"
 	       "\n"
-	       "  directon: set gpio direction: \"input\" or \"output\"\n"
-	       "  gpio_nr:  number of GPIO which will be exported \n"
-	       "  count:    set the number of loop iterations (default: 10)\n"
-	       "  delay:    set the delay in seconds between the set values"
-						"of the gpio (default: 1)\n"
-	       "\n"
-	       "If you want to use delay option you have to use count option "
-	       "too. So you cant use delay without using count\n"
+	       "  gpio:  GPIO index (see GPIO reference card)\n"
+	       "  dir:   Direction, one of 'in', 'out', 'high', 'low' (default 'in')\n"
+	       "  count: Number of loop iterations (default 10)\n"
+	       "  delay: Delay in seconds between loop iterations (default 1)\n"
 	       "\n", progname);
-}
-
-
-/*****************************************************************************
-*** Function:    void test_output()                                        ***
-***                                                                        ***
-*** Parameters:  gpio: GPIO Value as String                                ***
-***		 count: number of loop interations			   ***
-***		 delay: delay in seconds between set values		   ***
-***                                                                        ***
-*** Return:      -                                                         ***
-***                                                                        ***
-*** Description                                                            ***
-*** -----------                                                            ***
-*** This function tests GPIO ports as output                               ***
-*****************************************************************************/
-void test_output(const char *gpio, int count, int delay)
-{
-	int i = 0;
-
-	createGPIO(gpio);
-	setDirection(gpio, "out");
-
-	do
-	{
-		setValue(gpio, "1");
-		sleep(delay);
-		setValue(gpio, "0");
-		sleep(delay);
-		i++;
-	} while(count != i);
-}
-
-
-/*****************************************************************************
-*** Function:    void test_input()                                         ***
-***                                                                        ***
-*** Parameters:  gpio: GPIO Value as String                                ***
-***		 count: number of loop interations			   ***
-***		 delay: delay in seconds between get value		   ***
-***                                                                        ***
-*** Return:      -                                                         ***
-***                                                                        ***
-*** Description                                                            ***
-*** -----------                                                            ***
-*** This function tests GPIO ports as input.                               ***
-*****************************************************************************/
-void test_input(const char *gpio, int count, int delay)
-{
-	int i = 0;
-	int gpio_stored = 49;
-	int gpio_val = NULL;
-
-	createGPIO(gpio);
-	setDirection(gpio, "in");
-	printf("Input value %d\n", (getValue(gpio) - 48));
-
-	do
-	{
-		sleep(delay);
-		gpio_val = getValue(gpio);
-		if(gpio_val != gpio_stored) {
-			gpio_stored = gpio_val;
-			printf("Input value changed %d\n",
-						(getValue(gpio) - 48));
-		}
-		i++;
-	} while(count != i);
 }
 
 
@@ -295,44 +110,92 @@ void test_input(const char *gpio, int count, int delay)
 ***                                                                        ***
 *** Description                                                            ***
 *** -----------                                                            ***
-*** Parse the command line options and call the necessary functions to     ***
-*** use GPIO                                                               ***
+*** Parse the command line options and handle GPIO.                        ***
 *****************************************************************************/
 int main(int argc, const char *argv[])
 {
-	/* initialize values and set default parameters */
-	const char *gpio = NULL;
+	unsigned int gpio;
 	unsigned int count = 10;
 	unsigned int delay = 1;
+	int direction_in;
+	int i;
+	int value;
+	const char *direction = "in";
+	DIR *dir_gpio;
+	FILE *file;
 
-	/* test command line arguments */
-	if ((argc < 3) || (argc > 5)) {
+	/* Parse command line arguments */
+	if ((argc < 2) || (argc > 5)) {
 		usage(argv[0]);
 		return 1;
 	}
-	gpio =argv[2];
-	if(gpio == NULL) {
-		usage(argv[0]);
-		return 1;
-	}
-
-	if(argc > 3)
+	gpio = strtoul(argv[1], NULL, 0);
+	if (argc > 2)
+		direction = argv[2];
+	if (argc > 3)
 		count = strtoul(argv[3], NULL, 0);
-		if(count == 0)
-			printf("Please note you have an endless loop. Abort"
-			" with strg-c\n");
-
-	if(argc > 4)
+	if (argc > 4)
 		delay = strtoul(argv[4], NULL, 0);
+	direction_in = (strcmp(direction, "in") == 0);
+	if (!count)
+		count = INT_MAX;
 
-	if (strcmp((argv[1]),"input")==0)
-		test_input(gpio, count, delay);
-	else if (strcmp((argv[1]),"output")==0)
-		test_output(gpio, count, delay);
-	else {
-		usage(argv[0]);
-		return 1;
+	/* If gpio is not exported yet, export it now. Open gpio directory. */
+	sprintf(path_gpio, "%s/gpio%d", GPIO_PATH, gpio);
+	dir_gpio = opendir(path_gpio);
+	if (!dir_gpio) {
+		if (errno != ENOENT)
+			return show_error("Can not access gpio", path_gpio);
+
+		file = fopen(EXPORT_PATH, "w");
+		if (!file)
+			return show_error("Can not open export", EXPORT_PATH);
+		if ((fprintf(file, "%d", gpio) < 0) || (fclose(file) == EOF))
+			return show_error("Can not export gpio", EXPORT_PATH);
+
+		dir_gpio = opendir(path_gpio);
+		if (!dir_gpio)
+			return show_error("Can not access gpio", path_gpio);
 	}
+
+	/* Set direction */
+	sprintf(path, "%s/direction", path_gpio);
+	file = fopen(path, "w");
+	if (!file)
+		return show_error("Can not access direction", path);
+	if ((fprintf(file, "%s", direction) < 0) || (fclose(file) == EOF))
+		return show_error("Can not set direction", path);
+
+	/* Open value stream in unbuffered mode */
+	sprintf(path, "%s/value", path_gpio);
+	file = fopen(path, direction_in ? "r" : "w");
+	if (!file)
+		return show_error("Can not open value", path);
+	if (setvbuf(file, NULL, _IONBF, 0) != 0)
+		return show_error("Can not set file to unbuffered mode", path);
+
+	/* Handle loop */
+	i = 0;
+	do {
+		if (i)
+			sleep(delay);
+		i++;
+		if (direction_in) {
+			value = -1;
+			fseek(file, 0, SEEK_SET);
+			fscanf(file, "%d", &value);
+			printf("Loop %i: Value is %d\n", i, value);
+		} else {
+			printf("Loop %i: Setting value 1\n", i);
+			fprintf(file, "1");
+			sleep(delay);
+			printf("Loop %i: Setting value 0\n", i);
+			fprintf(file, "0");
+		}
+	} while (i < count);
+
+	fclose(file);
+	closedir(dir_gpio);
 
 	return 0;
 }
